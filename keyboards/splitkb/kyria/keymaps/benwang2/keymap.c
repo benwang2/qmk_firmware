6530250f84903e47ce7f2671f4f7377b9f093487
benwang2 <eb.benwang@gmail.com>
2022-05-31 11:35:47 -0400

    Updated files.

--- keyboards/splitkb/kyria/keymaps/benwang2/keymap.c
+++ keyboards/splitkb/kyria/keymaps/benwang2/keymap.c
@@ -19,7 +19,7 @@ enum layers {
     _QWERTY = 0,
     // _DVORAK,
     _COLEMAK_DH,
-    _NAV,
+    _FN_NAV,
     _SYM,
     // _FUNCTION,
     _ADJUST,
@@ -32,7 +32,7 @@ enum layers {
 #define DVORAK   DF(_DVORAK)
 
 #define SYM      MO(_SYM)
-#define NAV      MO(_NAV)
+#define FN_NAV      MO(_FN_NAV)
 // #define FKEYS    MO(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
 
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_LSFT , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
      KC_LCTL, KC_Z ,  KC_X    ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,         KC_CAPS , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT , KC_SLASH, KC_ENT,
-                                ADJUST , KC_LGUI, KC_LALT, KC_SPC , SYM   ,         NAV     , KC_SPC , KC_RALT, _______, KC_APP
+                                ADJUST , KC_LGUI, KC_LALT, KC_SPC , SYM   ,         FN_NAV  , KC_SPC , KC_RALT, _______, KC_APP
     ),
     
 /*
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSPC,
      KC_LSFT , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_QUOT,
      KC_LCTL , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LBRC,KC_CAPS,     KC_CAPS  , KC_RBRC, KC_K, KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_ENT,
-                                 ADJUST, KC_LGUI, KC_LALT, KC_SPC , SYM   ,     NAV    , KC_SPC ,KC_RALT, _______, KC_APP
+                                 ADJUST, KC_LGUI, KC_LALT, KC_SPC , SYM   ,     FN_NAV   , KC_SPC ,KC_RALT, _______, KC_APP
     ),
 
 /*
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_NAV] = LAYOUT(
+    [_FN_NAV] = LAYOUT(
       KC_ESC ,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
       _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
       _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, KC_SLCK, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
@@ -248,7 +248,7 @@ bool oled_task_user(void) {
             case _COLEMAK_DH:
                 oled_write_P(PSTR("Colemak-DH\n"), false);
                 break;
-            case _NAV:
+            case _FN_NAV:
                 oled_write_P(PSTR("Fn-Nav\n"), false);
                 break;
             case _SYM:
